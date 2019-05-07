1acb46c3f62c292993904907c6bf6dc590372b39
Chanon Jenakom <chanonjenakom@gmail.com>
2019-05-07 15:46:35 +0700

    Create base class for fading cluster

--- estream/__init__.py
+++ estream/__init__.py
@@ -1,1 +1,2 @@
+from estream.fading_cluster import FadingCluster
 from estream.histogram import Histogram
---
+++ estream/fading_cluster.py
@@ -0,0 +1,69 @@
+from estream.histogram import Histogram
+
+class FadingCluster:
+
+    """
+    ID counter
+    """
+    id_counter = 0
+
+    """
+    Constructor
+    """
+    def __init__(self, vector):
+        # Cluster ID
+        self.id = FadingCluster.id_counter
+        FadingCluster.id_counter += 1
+        # Public fields
+        self.is_active = False
+        self.weight = 1.0
+        self.dimension = len(vector)
+        self.LS = [value for value in vector]
+        self.SS = [value ** 2 for value in vector]
+        self.histograms = []
+        for value in vector:
+            histogram = Histogram()
+            histogram.add(value)
+            self.histograms.append(histogram)
+    
+    """
+    Overridden methods
+    """
+    def __copy__(self):
+        pass
+    
+    def __deepcopy__(self, memo):
+        pass
+    
+    """
+    Public methods
+    """
+    def center(self):
+        pass
+    
+    def sd(self):
+        pass
+    
+    def get_center_distance(self, other):
+        pass
+    
+    def get_normalized_distance(self, vector):
+        pass
+    
+    def fade(self, fading_factor):
+        pass
+    
+    def can_split(self):
+        pass
+    
+    def split(self):
+        pass
+    
+    def merge(self, other):
+        pass
+    
+    def is_overlapped(self, other, threshold):
+        pass
+    
+    def add(self, vector):
+        pass
