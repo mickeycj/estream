cb12f05e9f54e9bf4594c36d3fddeaeefc8e51ae
Chanon Jenakom <chanonjenakom@gmail.com>
2019-05-10 17:51:24 +0700

    Update specs for checking split in fading cluster

--- tests/fading_cluster/can_split_spec.py
+++ tests/fading_cluster/can_split_spec.py
@@ -22,6 +22,30 @@ with description('Fading Cluster:') as self:
 
     with context('When check whether the fading cluster can be splitted'):
 
+        with context('and the split does not appear,'):
+
+            with before.all:
+                FadingCluster.id_counter = 0
+
+                self.fading_cluster = FadingCluster([2.0, 1.0])
+                self.add(self.fading_cluster, [2.0, 1.0])
+                self.add(self.fading_cluster, [2.0, 1.0])
+                self.add(self.fading_cluster, [2.0, 1.0])
+                self.add(self.fading_cluster, [2.0, 1.0])
+
+                self.split_index, self.split_attribute = self.fading_cluster.can_split()
+            
+            with it('should return the split index of -1.'):
+                expect(self.split_index).to(equal(-1))
+            
+            with it('should return the split attribute of -1.'):
+                expect(self.split_attribute).to(equal(-1))
+            
+            with after.all:
+                del self.fading_cluster
+                del self.split_index
+                del self.split_attribute
+
         with context('and the split appears first,'):
 
             with before.all:
