fc9849e62d8f01988991aa2edaef06e3c80d60b8
Chanon Jenakom <chanonjenakom@gmail.com>
2019-05-11 02:05:13 +0700

    Implement method for splitting fading cluster

--- estream/fading_cluster.py
+++ estream/fading_cluster.py
@@ -102,7 +102,54 @@ class FadingCluster:
         return -1, -1
     
     def split(self, split_index, split_attribute):
-        pass
+        if split_index == -1 or split_attribute == -1:
+            return None
+
+        lower_weight, higher_weight = 0.0, 0.0
+        for idx, height in enumerate(self.histograms[split_attribute].heights):
+            if idx <= split_index:
+                lower_weight += height
+            else:
+                higher_weight += height
+        if lower_weight > higher_weight:
+            lower_weight, higher_weight = higher_weight, lower_weight
+        
+        new_fading_cluster = FadingCluster.from_fading_cluster(self)
+        lower_factor, higher_factor = lower_weight / self.weight, higher_weight / self.weight
+        for idx in range(self.dimension):
+            if idx == split_attribute:
+                new_fading_cluster.histograms[idx] = self.histograms[idx].split(split_index)
+
+                histogram = self.histograms[idx]
+                new_histogram = new_fading_cluster.histograms[idx]
+
+                self.LS[idx], self.SS[idx] = 0.0, 0.0
+                new_fading_cluster.LS[idx], new_fading_cluster.SS[idx] = 0.0, 0.0
+                half_width, new_half_width = histogram.width * 0.5, new_histogram.width * 0.5
+                for height, upper_bound, new_height, new_upper_bound in zip(histogram.heights,
+                                                                            histogram.upper_bounds,
+                                                                            new_histogram.heights,
+                                                                            new_histogram.upper_bounds):
+                    position = upper_bound - half_width
+                    self.LS[idx] += height * position
+                    self.SS[idx] += height * position ** 2
+
+                    new_position = new_upper_bound - new_half_width
+                    new_fading_cluster.LS[idx] += new_height * new_position
+                    new_fading_cluster.SS[idx] += new_height * new_position ** 2
+            else:
+                self.LS[idx] *= higher_factor
+                self.SS[idx] *= higher_factor
+                self.histograms[idx].heights = [height * higher_factor for height
+                                                in self.histograms[idx].heights] 
+                new_fading_cluster.LS[idx] *= lower_factor
+                new_fading_cluster.SS[idx] *= lower_factor
+                new_fading_cluster.histograms[idx].heights = [height * lower_factor for height
+                                                              in new_fading_cluster.histograms[idx].heights]
+        self.weight = higher_weight
+        new_fading_cluster.weight = lower_weight    
+
+        return new_fading_cluster
     
     def merge(self, other):
         pass
