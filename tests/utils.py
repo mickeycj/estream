671287fb5d5abe51c2bccee4febeca84586de99b
Chanon Jenakom <chanonjenakom@gmail.com>
2019-05-23 14:51:07 +0700

    Document test utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,12 @@
-def add_vector(cluster, vector):
-    fading_factor = 0.99
+"""
+Mocked fading factor
+"""
+fading_factor = 0.99
 
+"""
+Mocked method for adding vectors
+"""
+def add_vector(cluster, vector):
     cluster.weight *= fading_factor
     cluster.LS = [ls * fading_factor for ls in cluster.LS]
     cluster.SS = [ss * fading_factor for ss in cluster.SS]
